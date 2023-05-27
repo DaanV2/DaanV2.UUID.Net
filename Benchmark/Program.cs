2d3306ee3a17847def354be685cdda57771a0225
DaanV2 <daanverstraten@hotmail.com>
2023-05-27 18:02:45 +0200

    Updated

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Benchmark;
+using Benchmark.Generation;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
+using DaanV2.UUID;
 
 public partial class Program {
     private static void Main(String[] args) {
@@ -10,3 +12,13 @@ public partial class Program {
         BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
     }
 }
+
+//Console.ReadLine();
+//Console.WriteLine("Start");
+//var Test = new AllVersions();
+//var uuids = new UUID[50_000_000];
+
+//for (Int32 i = 0; i < uuids.Length; i++) {
+//    uuids[i] = Test.V1();
+//}
+//Console.WriteLine("Done");
