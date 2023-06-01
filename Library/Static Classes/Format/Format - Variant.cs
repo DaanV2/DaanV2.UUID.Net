2ce33e03c2487adae026a27827270e7edb616e01
Daan Verstraten <daanverstraten@hotmail.com>
2023-06-01 18:31:59 +0200

    Handling platform supported (#7)
    * Using System.Numerics.BitOperations, as interestic will throw platform exception, while this one checks which to use
    * Upgraded to version 2.0.2

--- Library/Static Classes/Format/Format - Variant.cs
+++ Library/Static Classes/Format/Format - Variant.cs
@@ -56,9 +56,8 @@ public static partial class Format {
         // 1 0 x. 1 bits => 0b1100_0000
         // 1 1 0. 2 bits => 0b1110_0000
         // 1 1 1. 3 bits => 0b1110_0000
-
-        UInt32 bitsSet = System.Runtime.Intrinsics.X86.Popcnt.PopCount((UInt32)variant);
-        Int32 r = (Int32)(bitsSet + 1);
+        Int32 bitsSet = System.Numerics.BitOperations.PopCount((UInt32)variant);
+        Int32 r = bitsSet + 1;
         if (r > 3) {
             r = 3;
         }
