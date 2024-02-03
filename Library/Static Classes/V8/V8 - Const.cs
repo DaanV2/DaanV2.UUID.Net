707f10e17b85611c984cc1c9fad00c7f88baa1e2
DaanV2 <daanverstraten@hotmail.com>
2024-02-03 14:13:35 +0100

    Adding V8

---
+++ Library/Static Classes/V8/V8 - Const.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Intrinsics;
+using System.Security.Cryptography;
+
+namespace DaanV2.UUID;
+public static partial class V8 {
+    /// <inheritdoc cref="V1.Version"/>
+    public const Version Version = DaanV2.UUID.Version.V8;
+    /// <inheritdoc cref="V1.Variant"/>
+    public const Variant Variant = DaanV2.UUID.Variant.V1;
+
+    private static readonly Vector128<Byte> _VersionMask = Format.VersionVariantMaskNot(V8.Version, V8.Variant);
+    private static readonly Vector128<Byte> _VersionOverlay = Format.VersionVariantOverlayer(V8.Version, V8.Variant);
+}
