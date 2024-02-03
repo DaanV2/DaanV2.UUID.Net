d7269f3e921c05a4dff23d48f015e65cc023e63e
DaanV2 <daanverstraten@hotmail.com>
2024-02-03 14:15:39 +0100

    Adding the base for v8

---
+++ Library/Static Classes/V8/V8.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaanV2.UUID;
+
+/// <summary>The <see cref="UUID"/> version that is based on given data of 122 bits</summary>
+public static partial class V8 {
+}
