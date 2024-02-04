c3dc4c65843730e448b2591bc7c9892fc793dd74
DaanV2 <daanverstraten@hotmail.com>
2024-02-04 17:57:10 +0100

    Adding more tests

--- Tests/Generation/V8Tests.cs
+++ Tests/Generation/V8Tests.cs
@@ -2,6 +2,21 @@
 
 namespace Tests.Generation;
 public sealed partial class V8Tests {
+    // From https://www.ietf.org/archive/id/draft-peabody-dispatch-new-uuid-format-04.html#name-example-of-a-uuidv8-value
+    [Theory(DisplayName = "Given a known vector, will generate the expected UUID")]
+    [InlineData(0x320C3D4DCC00, 0x75B, 0xEC932D5F69181C0, "320c3d4d-cc00-875b-8ec9-32d5f69181c0")]
+    public void TestVector(UInt64 customA, UInt16 customB, UInt64 customC, String expected) {
+        UUID u = V8.Generate(customA, customB, customC);
+        Assert.Equal(expected, u.ToString());
+
+        Utility.ValidateUUID(u, V8.Version, V8.Variant);
+
+        (UInt64 bits48, UInt16 bits12, UInt64 bits62) = V8.Extract(u);
+        Assert.Equal(customA, bits48);
+        Assert.Equal(customB, bits12);
+        Assert.Equal(customC, bits62);
+    }
+
     [Theory(DisplayName = "When batch generating, will always return unique UUIDs")]
     [InlineData(10)]
     [InlineData(100)]
