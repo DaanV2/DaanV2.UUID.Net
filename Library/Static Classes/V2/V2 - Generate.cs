b10a20a3cdb86be6b4fdfbd4c1da0d44a1c12271
Daan Verstraten <daanverstraten@hotmail.com>
2025-09-18 12:40:15 +0000

    fix: fixing constants

--- Library/Static Classes/V2/V2 - Generate.cs
+++ Library/Static Classes/V2/V2 - Generate.cs
@@ -32,7 +32,7 @@ public static partial class V2 {
         uint timeLow = (uint)(uuidTimestamp & 0xFFFFFFFF);
         ushort timeMid = (ushort)((uuidTimestamp >> 32) & 0xFFFF);
         ushort timeHi = (ushort)((uuidTimestamp >> 48) & 0x0FFF); // 12 bits for time_hi
-        ushort timeHiAndVersion = (ushort)(timeHi | (_Version << 12)); // Set version 2
+        ushort timeHiAndVersion = (ushort)(timeHi | ((ushort)Version.V2)); // Set version 2
 
         BinaryPrimitives.WriteUInt32BigEndian(data, timeLow);
         BinaryPrimitives.WriteUInt16BigEndian(data[4..], timeMid);
@@ -41,7 +41,7 @@ public static partial class V2 {
         // Per RFC 4122 DCE Security UUID:
         // data[8]: high 2 bits = variant, low 6 bits = high 6 bits of local identifier
         // data[9]: domain (per spec), or low 8 bits of local identifier if domain not used
-        data[8] = (byte)(_Variant | ((localIdentifier >> 8) & 0x3F));
+        data[8] = (byte)(((localIdentifier >> 8) & 0x3F) | (byte)Variant.V1);
         data[9] = domain; // Store domain in data[9]
 
         var uuid = Vector128.Create<Byte>(data);
@@ -68,8 +68,8 @@ public static partial class V2 {
         Span<byte> data = stackalloc byte[16];
         System.Security.Cryptography.MD5.HashData(source, data);
         // Set version and variant using V2.Version and V2.Variant constants
-        data[6] = (byte)((data[6] & 0x0F) | ((byte)V2.Version));
-        data[8] = (byte)((data[8] & 0x3F) | (byte)V2.Variant);
+        data[6] = (byte)((data[6] & 0x0F) | (byte)Version.V2);
+        data[8] = (byte)((data[8] & 0x3F) | (byte)Variant.V1);
 
         var uuid = Vector128.Create<Byte>(data);
         return new UUID(uuid);
